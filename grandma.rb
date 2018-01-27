955cec808aaa4c033fbf196fcd4931aefb598cab
Katie Cassell <kcassell944@gmail.com>
2018-01-27 20:20:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-  end
   return "I LOVE YOU TOO PUMPKIN!"
-
+end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
