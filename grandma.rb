1e735d762005bd3cc7b87f37746825034f580dbc
Katie Cassell <kcassell944@gmail.com>
2018-01-27 21:05:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif phrase.uppercase == phrase
-  return "HUH?! SPEAK UP, SONNY!"
+elsif phrase.upcase == phrase
+  return "NO, NOT SINCE 1938!"
 end
 end
 
